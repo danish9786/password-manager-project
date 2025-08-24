fb36f399e191ba9a3c90b288f185d35aa7acb053
DANISH <mdastudies84478@gmail.com>
2025-08-24 05:16:07 +0000

    commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,18 @@
+import Navbar from './components/Navbar'
+import './App.css'
+import Manager from './components/manager'
+import Footer from './components/Footer'
+
+function App() {
+
+
+  return (
+    <>
+      <Navbar />
+      <Manager/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
